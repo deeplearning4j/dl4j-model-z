7c3ca12e14c36411dc8940a37328b424bda73b50
Justin Long <justinlong@outlook.com>
2016-12-28 16:37:19 -0800

    Import activation.

--- src/main/java/org/deeplearning4j/FaceNetVariant.java
+++ src/main/java/org/deeplearning4j/FaceNetVariant.java
@@ -14,6 +14,7 @@ import org.deeplearning4j.nn.conf.inputs.InputType;
 import org.deeplearning4j.nn.conf.layers.*;
 import org.deeplearning4j.nn.graph.ComputationGraph;
 import org.deeplearning4j.nn.weights.WeightInit;
+import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.lossfunctions.LossFunctions;
 
 /**
